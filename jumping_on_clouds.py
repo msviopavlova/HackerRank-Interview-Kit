07123fea9eb63328af300f96553498ec6180028f
Violetta Pavlova <msviopavlova@gmail.com>
2021-06-10 22:44:36 +0100

    solved

--- jumping_on_clouds.py
+++ jumping_on_clouds.py
@@ -1,25 +1,24 @@
 
+
 def clouds(c):
     jumps = 0
     current = 0
 
-    for _ in c:
-        if (current + 1) < len(c)-2:
-            if c[current + 2] == 0:
-                current = current + 2
-                jumps = jumps + 1
-            else:
-                current = current + 1
-                jumps = jumps + 1
-        elif current == len(c) - 1:
-            if c[current] == 0:
-                jumps = jumps +1
-                current = current + 1
+    while current < len(c)-1:
+        if current+2 <= len(c)-1 and c[current + 2] == 0:
+            current = current + 2
+            jumps = jumps + 1
+        else:
+            current = current + 1
+            jumps = jumps + 1
 
     return jumps
 
 
-c = [0, 0, 0, 1, 0, 0]
+
+
+c = [0, 0, 1, 0, 0, 1, 0]
+
 
 print(clouds(c))
 
