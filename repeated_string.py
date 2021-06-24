78bc4ded4e052aa68495d941b7f28b6b8184d2b2
Violetta Pavlova <msviopavlova@gmail.com>
2021-06-24 16:55:15 +0100

    repeated string code passed all cases finally!

--- repeated_string.py
+++ repeated_string.py
@@ -1,18 +1,42 @@
-import math
+# import math
+# def repeatedString(s, n):
+#     # Write your code here
+#     repetition = math.ceil(n/len(s))
+#     print(f"{repetition} times fits in {n}")
+#     r = s*repetition
+#     print(f"{r} this is r var")
+#     # s_new = r[:n]
+#     # print(f"{s_new} new string")
+#     result = []
+#     for char in r:
+#         if char == "a":
+#             result.append(char)
+#    # print(result)
+#     LONG_INTEGER = len(result)
+#     return LONG_INTEGER
+
+
 def repeatedString(s, n):
     # Write your code here
-    repetition = math.ceil(n/len(s))
-    r = s*repetition
-    s_new = r[:n]
-    print(f"{s_new} new string")
-    result = []
-    for char in s_new:
+    letters = []
+    for char in s:
         if char == "a":
-            result.append(char)
-   # print(result)
-    LONG_INTEGER = len(result)
-    return LONG_INTEGER
+            letters.append(char)
+    leftover = n % len(s)
+    print(f"{leftover} modulos")
+    times = n // len(s)
+    amountofains = len(letters)
+    extras = []
+    for d in s[:leftover]:
+        if d == "a":
+            extras.append(d)
+
+    result = (amountofains * times) + len(extras)
+    return result
 
 
 
-print(repeatedString("a", 100))
+
+
+
+print(repeatedString("aba", 10))
